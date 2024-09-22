5db1bad7b77ce34478fc50844c8dfe39747c0721
epochlab <JRoberts86@gmail.com>
2024-09-22 21:22:57 +0100

    Linear decay, in-place update and limit FPS

--- particle.py
+++ particle.py
@@ -3,15 +3,15 @@
 import numpy as np
 
 class ParticleField:
-    def __init__(self, n_particles, width, height, dt):
-        self.n_particles = n_particles
+    def __init__(self, N, width, height, dt):
+        self.N = N
         self.width = width
         self.height = height
         self.dt = dt
 
-        self.pos = np.random.rand(n_particles, 2) * [width, height]
-        self.vel = (np.random.rand(n_particles, 2) - 0.5) * 10
-        self.life = np.ones(n_particles)
+        self.pos = np.random.rand(N, 2) * [width, height]
+        self.vel = (np.random.rand(N, 2) - 0.5) * 1000
+        self.life = np.ones(N)
         
         self.state = np.hstack((self.pos, self.vel, self.life[:, None]))
 
@@ -19,14 +19,17 @@ class ParticleField:
         pos, vel, life = state[:, :2], state[:, 2:4], state[:, 4]
 
         vel = self.boundary_collisions(vel, pos)
+        life = self.linear_decay(life)
 
         # Derivatives
         dv_dt = vel # vel
         da_dt = np.zeros_like(vel) # accel
-        dl_dt = -np.ones_like(life) * self.dt * 20 # linear decay
+        dl_dt = life
 
         return np.hstack((dv_dt, da_dt, dl_dt[:, None]))
     
+    def linear_decay(self, life): return -np.ones_like(life) * self.dt
+        
     def boundary_collisions(self, vel, pos):
         vel[(pos[:, 0] <= 0) | (pos[:, 0] >= self.width), 0] *= -1
         vel[(pos[:, 1] <= 0) | (pos[:, 1] >= self.height), 1] *= -1
--- run.py
+++ run.py
@@ -10,9 +10,10 @@ from particle import ParticleField
 np.set_printoptions(precision=2, suppress=True)
 
 def main():
-    WIDTH, HEIGHT = 1024, 576
+    WIDTH, HEIGHT, FPS = 1024, 576, 120
     render = Display(WIDTH, HEIGHT)
-    dt = 1/24
+    clock = pygame.time.Clock()
+    dt = 1/FPS
 
     P = ParticleField(10, WIDTH, HEIGHT, dt)
     solver = ODESolver(f=P.dynamics)
@@ -23,11 +24,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        next_state, _ = solver.compute(dt, "euler")  # Simulate
-        render.draw(next_state, (255, 255, 255))     # Render
-        P.state = next_state                         # Update article field
-
+        next_state, _ = solver.compute(dt, "euler")
+        render.draw(next_state, (255, 255, 255))
+        P.state[:] = next_state
         print(P.state)
+        
+        clock.tick(FPS)
 
 if __name__ == "__main__":
     main()
