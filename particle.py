0b35536be91eec2c0140c0becfb76395a1a5c8df
epochlab <JRoberts86@gmail.com>
2024-09-25 19:50:30 +0100

    Refactor initial conditions

--- particle.py
+++ particle.py
@@ -6,25 +6,26 @@ class ParticleField:
     def __init__(self, N, profile, width, height, dt, device):
         self.N = N
         self.profile = profile
+
         self.width = width
         self.height = height
         self.dt = dt
         self.device = device
 
-        self.pos = (torch.rand(N, 2) * torch.tensor([width, height])).to(device)
-        self.vel = (torch.rand(N, 2) - 0.5).to(device) * 1000
+        self.state = self._initialize()
 
-        self.state = torch.cat((self.pos, self.vel), dim=1)
+    def _initialize(self):
+        pI = (torch.rand(self.N, 2) * torch.tensor([self.width, self.height])).to(self.device)
+        vI = (torch.rand(self.N, 2) - 0.5).to(self.device) * 1000
+        return torch.cat((pI, vI), dim=1)
 
     def dynamics(self, state, t):
-        pos, vel = state[:, :2], state[:, 2:4]
-        
-        vel = self.boundary_collisions(vel, pos)
+        vel = self.boundary_collisions()
         dist = self.distance()
-
         return torch.cat((vel, torch.zeros_like(vel)), dim=1) # dx_dt | p:vel, v:accel
         
-    def boundary_collisions(self, vel, pos):
+    def boundary_collisions(self):
+        pos, vel = self.state[:, :2], self.state[:, 2:]
         mask_x = (pos[:, 0] <= 0) | (pos[:, 0] >= self.width)
         mask_y = (pos[:, 1] <= 0) | (pos[:, 1] >= self.height)
         vel[mask_x, 0] *= -1
